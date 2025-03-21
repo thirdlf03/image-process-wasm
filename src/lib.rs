2b1fce184536603cc79999ee11e0bd9810b2d1a4
thirdlf03 <thirdlf03@gmail.com>
2025-03-21 22:08:56 +0900

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
 use std::io::Cursor;
 
 use base64::prelude::*;
-use image::load_from_memory;
+use image::{imageops::FilterType::Nearest, load_from_memory};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn edit_image(base_img: &str) -> String {
+pub fn edit_image(base_img: &str, select: &str) -> String {
     //base64 to image
     let img_buffer = BASE64_STANDARD.decode(base_img).unwrap();
     let mut img = load_from_memory(img_buffer.as_slice()).unwrap();
 
-    //grayscale
-    img = img.grayscale();
+    img = match select {
+        "half" => img.resize(img.width() / 2, img.height() / 2, Nearest),
+        "gray" => img.grayscale(),
+        "r90" => img.rotate90(),
+        "r180" => img.rotate180(),
+        _ => img,
+    };
 
     //image to base64
     let mut buf: Vec<u8> = Vec::new();
